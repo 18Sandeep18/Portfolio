3c55c795f80c8906da54166c43d92699f4556ea4
Sandeep Rishi <rishisandeep8@gmail.com>
2024-07-22 18:33:31 +0530

    files added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from './Components/About/About'
 import Experience from './Components/Experience/Experience'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 
 
