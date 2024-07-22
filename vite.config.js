2a30ebb9bc670665823a357ffe840453af0b4453
Sandeep Rishi <rishisandeep8@gmail.com>
2024-07-22 18:43:59 +0530

    files modified

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio/', // Ensure this matches your GitHub Pages repo name
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // Ensure the build output directory is set correctly
+  },
+});
