e314299e8c3dce6eeee684c29d8b185e3ebb2b57
Sandeep Rishi <rishisandeep8@gmail.com>
2025-04-24 12:30:03 +0530

    vite changed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Portfolio/', // Ensure this matches your GitHub Pages repo name
+  base: './', // Ensure this matches your GitHub Pages repo name
   plugins: [react()],
   build: {
     outDir: 'build', // Ensure the build output directory is set correctly
